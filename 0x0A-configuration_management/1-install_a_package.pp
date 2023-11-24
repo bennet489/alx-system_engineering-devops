cea9ed719da394c250fce67049aa6343da1ed50b
bennet489 <bennetasare@outlook.com>
2023-11-25 01:51:27 +0300

    task 1 bug fixed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,10 @@
-#!/usr/bin/pup
 # installs flask from pip3
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
 package { 'werkzeug':
-ensure => '2.1.1',
-provider => 'pip3',
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
 
